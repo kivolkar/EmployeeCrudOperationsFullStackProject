93ce44d39979d4fb9bd1b479f84ef147603bfe1d
kivolkar <kivolkar@gmail.com>
2025-02-23 16:27:26 +0530

    Initial commit

---
+++ src/main/java/com/example/EmployeeCrudOpearations/EmployeeCrudOpearationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeCrudOpearations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudOpearationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudOpearationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EmployeeCrudOpearations/EmployeeCrudOpearationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeCrudOpearations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudOpearationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
